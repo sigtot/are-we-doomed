f922bd63b9cc9a0bfb8a5dd039cdde63d37e29d9
sigtot <sigurdtotland@gmail.com>
2018-10-12 14:24:51 +0200

    Create vue app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MainView/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainView from './components/MainView.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MainView
   }
 }
 </script>
---
+++ src/components/MainView.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="main-view">
+        <h1>Hey</h1>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name: "MainView",
+    data () {
+        return {
+            asteroids: null
+        }
+    },
+    mounted () {
+        axios.get("https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-09-07&api_key=API_KEY")
+            .then(response => (this.asteroids = response))
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
